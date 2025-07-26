4c9fa15b44ca99599e734f39677ef9b52fc3cd1c
binam01 <164865293+binam01@users.noreply.github.com>
2025-07-26 15:09:17 +0545

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Question {
+    private String question;
+    private String[] options;
+    private int answer;
+
+    public Question(String question, String[] options, int answer) {
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public String getQuestion() { return question; }
+    public String[] getOptions() { return options; }
+    public int getAnswer() { return answer; }
+}
